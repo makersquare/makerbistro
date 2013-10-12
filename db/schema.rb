cdee6d4170bb34a515874329beae1cf431c53820
nicole pank <nicole.pank@gmail.com>
2013-10-12 16:15:22 -0500

    create Ingredients model; add hirb gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131012205824) do
+ActiveRecord::Schema.define(version: 20131012211224) do
+
+  create_table "ingredients", force: true do |t|
+    t.string   "food_thing"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "menu_items", force: true do |t|
     t.string   "name"
