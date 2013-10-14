7d247bd19d1b9bc8667a1642e56689ba4b00021b
bangner <joswagne@gmail.com>
2013-10-13 23:25:00 -0500

    generated models for recipe and ingredient

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131014040737) do
+ActiveRecord::Schema.define(version: 20131014042123) do
+
+  create_table "ingredients", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "menu_items", force: true do |t|
     t.string   "name"
@@ -19,5 +25,15 @@ ActiveRecord::Schema.define(version: 20131014040737) do
     t.datetime "updated_at"
     t.string   "price"
   end
+
+  create_table "recipes", force: true do |t|
+    t.integer  "menu_item_id"
+    t.integer  "ingredient_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "recipes", ["ingredient_id"], name: "index_recipes_on_ingredient_id"
+  add_index "recipes", ["menu_item_id"], name: "index_recipes_on_menu_item_id"
 
 end
